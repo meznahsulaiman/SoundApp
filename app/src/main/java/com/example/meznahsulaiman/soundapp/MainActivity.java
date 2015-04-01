72e98a32483b70860d2ee77c0a009cce31bc90e5
meznah sulaiman <meznahSulaiman@meznahs-MacBook-Pro.local>
2015-04-01 10:56:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/meznahsulaiman/soundapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.meznahsulaiman.soundapp;
+
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    SoundPool mySound ;
+    int alertId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+
+        {
+            AudioAttributes aa = new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).setUsage(AudioAttributes.USAGE_GAME).build();
+
+            mySound = new SoundPool.Builder().setMaxStreams(10).setAudioAttributes(aa).build();
+
+            alertId = mySound.load(this, R.raw.alertsound, 1);
+        }
+        else
+        {
+
+           mySound = new SoundPool(10 ,AudioManager.STREAM_MUSIC,1);
+            alertId = mySound.load(this, R.raw.alertsound, 1);
+        }
+    }
+
+    public void playsounds(View view) {
+
+        mySound.play(alertId,1,1,1,0,1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
